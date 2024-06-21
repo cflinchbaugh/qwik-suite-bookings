a9d21460bb9ddecd4cff83bf0783ca29a36cad18
Christopher Flinchbaugh <chris.f@Very-MacBook-Pro.local>
2024-06-21 18:05:34 -0400

    [QWIK-001] Render the total cost

--- src/routes/bookings/index.tsx
+++ src/routes/bookings/index.tsx
@@ -1,6 +1,21 @@
 import { Resource, component$, useResource$ } from "@builder.io/qwik";
 import { Booking, fetchBookings } from "~/services/bookingsService";
 
+const convertCurrency = ({
+  currencyCode,
+  total,
+}: {
+  currencyCode: string;
+  total: number;
+}) => {
+  switch (currencyCode.toUpperCase()) {
+    case "USD":
+      return `$${total / 100}`;
+    default:
+      throw new Error(`Unsupported currency: ${currencyCode}`);
+  }
+};
+
 export default component$(() => {
   const bookingsData = useResource$(fetchBookings);
 
@@ -12,7 +27,39 @@ export default component$(() => {
         onPending={() => <div>Loading...</div>}
         onRejected={() => <div>Error</div>}
         onResolved={(data: Booking[]) =>
-          data.map((booking) => <div key={booking.id}>{booking.hotelName}</div>)
+          data.map((booking) => (
+            <div
+              style={{
+                border: "solid 1px",
+                borderRadius: "5px",
+                margin: "15px",
+                padding: "5px",
+              }}
+              key={booking.id}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: "5px",
+                }}
+              >
+                {booking.hotelName}
+
+                <div>
+                  {booking.checkInDate} - {booking.checkOutDate}
+                </div>
+
+                <div>
+                  Total:{" "}
+                  {convertCurrency({
+                    currencyCode: booking.currencyCode,
+                    total: booking.total,
+                  })}
+                </div>
+              </div>
+            </div>
+          ))
         }
       />
     </>
